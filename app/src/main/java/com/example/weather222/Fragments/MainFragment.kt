8760c835c3ae6eb6a2a2399eb9c1da101a25a855
thugballer <ma1lf0rw0rk@mail.ru>
2023-06-27 22:04:27 +0300

    second commit

---
+++ app/src/main/java/com/example/weather222/Fragments/MainFragment.kt
@@ -0,0 +1,50 @@
+package com.example.weather222.Fragments
+
+import android.Manifest
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.weather222.R
+import com.example.weather222.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
--- app/src/main/java/com/example/weather222/MainActivity.kt
+++ app/src/main/java/com/example/weather222/MainActivity.kt
@@ -6,37 +6,17 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.weather222.Fragments.MainFragment
 import com.example.weather222.databinding.ActivityMainBinding
 import org.json.JSONObject
 
-const val API_KEY = "488f532777b1a8e3e30fba36a239fb61"
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.bGet.setOnClickListener {
-            getResult("London")
-        }
-    }
-    private fun getResult(name: String){
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=voronezh&appid=$API_KEY"
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            {
-                    response->
-                val obj = JSONObject(response)
-                val temp = obj.getJSONObject("current")
-                Log.d("MyLog","Response: ${temp.getString("temp_c")}")
-            },
-            {
-                Log.d("MyLog","Volley error: $it")
-            }
-        )
-        queue.add(stringRequest)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
     }
 }
