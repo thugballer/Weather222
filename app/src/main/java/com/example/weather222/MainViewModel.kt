623b2bbc9cac3390eeb37dc3fe78cdebe7584cc2
thugballer <ma1lf0rw0rk@mail.ru>
2023-07-11 18:21:07 +0300

    last commit

--- app/src/main/java/com/example/weather222/MainViewModel.kt
+++ app/src/main/java/com/example/weather222/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.example.weather222
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weather222.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<String>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<WeatherModel>()
 }
